3d250d3f8e9eab1067ea2868f0e129f36585e800
Domi <holenstein.dominic@gmail.com>
2024-11-11 08:20:05 +0100

    sa

--- ls_ghost_hunt/link_check.lua
+++
@@ -1,3 +0,0 @@
-if GetResourceState('kq_link') ~= 'started' then
-    error('^6[KQ_LINK MISSING] ^1kq_link is required but not running! Make sure that you\'ve got it installed and started before ' .. GetCurrentResourceName())
-end
